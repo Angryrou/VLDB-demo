7382cf34b1e60327da3bc91f6c78287ac7382b7d
Chenghao <chenghao@cs.umass.edu>
2019-06-29 00:52:21 -0400

    set menu_i str

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-var default_menu_choice = 0;
+var default_menu_id = "0";
+var menu_choice;
 var menu_tab = {
-    0: 'workload',
-    1: 'execution-plan',
-    2: 'performance-models',
-    3: 'multi-objective-optimization',
-    4: 'net-benefits'
+    "0": 'workload',
+    "1": 'execution-plan',
+    "2": 'performance-models',
+    "3": 'multi-objective-optimization',
+    "4": 'net-benefits'
 };
 
 $(document).ready(function () {
-    $('a[data-tab=' + menu_tab[default_menu_choice] + ']').addClass('active');
+    $('a[data-tab=' + menu_tab[default_menu_id] + ']').addClass('active');
+    menu_id = default_menu_id;
 
     // control the menu tabs
     $('.ui .item').on('click', function () {
+        menu_choice = $(this).attr('menu_id');
         $('.ui .item').removeClass('active');
         $(this).addClass('active');
     });
